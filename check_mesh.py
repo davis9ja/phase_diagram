9510a0e46b5002f3800fc74cad2b082c066bd9f3
Jacob August Davison <daviso53@msu.edu>
2021-01-15 09:56:24 -0500

    print number of mesh points missing

--- check_mesh.py
+++ check_mesh.py
@@ -1,10 +1,10 @@
 import glob
 
-solvedGlob = glob.glob('solved_mesh/*.p')
-meshGlob = glob.glob('mesh_points/*.p')
+solvedGlob = glob.glob('target_es_experiment/solved_mesh/*.p')
+meshGlob = glob.glob('target_es_experiment/mesh_points/*.p')
 
-solvedList = [int((path.split('/')[1]).replace('.p', '')) for path in solvedGlob]
-meshList = [int((path.split('/')[1]).replace('.p', '')) for path in meshGlob]
+solvedList = [int((path.split('/')[2]).replace('.p', '')) for path in solvedGlob]
+meshList = [int((path.split('/')[2]).replace('.p', '')) for path in meshGlob]
 solvedList.sort()
 meshList.sort()
 
@@ -12,4 +12,6 @@ notList = []
 for case in meshList:
     if case not in solvedList:
         notList.append(str(case))
+
+print('{:3d} mesh points still missing\n'.format(len(notList)))
 print(','.join(notList))
